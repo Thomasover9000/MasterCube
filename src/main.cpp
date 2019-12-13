afe2db3998d31516ea5a7e55f770170cf499d225
Thomasover9000 <58548524+Thomasover9000@users.noreply.github.com>
2019-12-13 11:57:01 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include "cube.h"
+#include "debug.h"
+
+
 
 int main (int argc, char* argv[]) {
+	
+	//new 13.12.2019 adding debug
+	debug Debugger;
+	Debugger.setdebug(0);
+	Debugger.setdebug(1);
+	Debugger.printf("MasterCube is starting");
+	// Debug inilasition over
+	
+	
+	
+	
+	
 	Cube solved;
 	Cube scrambled;
 	
@@ -11,7 +26,6 @@ int main (int argc, char* argv[]) {
 */	
 	string s;
 	int n = 0;
-	
 	cin >> n >> s;
 	scrambled.splitQuestion(s, n);
 
